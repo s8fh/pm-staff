0452444725bb6fff1e53cc12420bcdecfe6572af
Jay Pfaffman <pfaffman@gmail.com>
2019-12-04 16:40:54 -0800

    now in variables!! it's personal, not private messages,

--- plugin.rb
+++ plugin.rb
@@ -4,15 +4,16 @@
 # authors: pfaffman
 
 after_initialize do
-  add_to_class('guardian', :can_send_private_message?) do |target|
-    target.is_a?(User) &&
+  add_to_class(:guardian, :can_send_personal_message?) do |target|
+    puts "u: #{target}, r: "
+    target.is_a?(User)  &&
       # User is authenticated
       authenticated? &&
-      # Have to be a basic level at least
+      # Have to be a basic level at least -- consider changing to require user level 1
       (target.staff? || @user.has_trust_level?(SiteSetting.min_trust_to_send_messages)) &&
       # User disabled private message
-      (target.staff? || is_staff? || target.user_option.allow_private_messages) &&
-      # PMs are enabled
-      (target.staff? || is_staff? || SiteSetting.enable_private_messages)
+      (target.staff? || is_staff? || target.user_option.allow_personal_messages) &&
+      # PMs are enabled -- TODO: remove target.staff? from this next line!!
+      (target.staff? || is_staff? || SiteSetting.enable_personal_messages)
   end
 end
