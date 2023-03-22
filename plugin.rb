4da674717e9f97464b70a8496b4c37b6a68b0937
Jay Pfaffman <jay@literatecomputing.com>
2023-03-22 11:05:57 -0700

    updates for new group setting

--- plugin.rb
+++ plugin.rb
@@ -6,24 +6,68 @@
 # authors: pfaffman
 
 after_initialize do
-  add_to_class(:guardian, :can_send_private_message?) do |target , notify_moderators: false|
-    is_user = target.is_a?(User)
-    is_group = target.is_a?(Group)
+  add_to_class(:guardian, :old_can_send_private_message?) do |target , notify_moderators: false|
+    # copied from guardian.rb
+    target_is_user = target.is_a?(User)
+    target_is_group = target.is_a?(Group)
+    from_system = @user.is_system_user?
 
-    (is_group || is_user || target.id < 0) &&
+    # Must be a valid target
+    return false if !(target_is_group || target_is_user)
+
+    # Users can send messages to certain groups with the `everyone` messageable_level
+    # even if they are not in personal_message_enabled_groups
+    group_is_messageable = target_is_group && Group.messageable(@user).where(id: target.id).exists?
+
+    # User is authenticated and can send PMs, this can be covered by trust levels as well via AUTO_GROUPS
+    (can_send_private_messages?(notify_moderators: notify_moderators) || group_is_messageable) &&
+      # User disabled private message
+      (is_staff? || target_is_group || target.user_option.allow_private_messages) &&
+      # Can't send PMs to suspended users
+      (is_staff? || target_is_group || !target.suspended?) &&
+      # Check group messageable level
+      (from_system || target_is_user || group_is_messageable || notify_moderators) &&
+      # Silenced users can only send PM to staff
+      (!is_silenced? || target.staff?)
+  end
+  add_to_class(:guardian, :can_send_private_messages?) do |notify_moderators: false|
+    from_system = @user.is_system_user?
+    from_bot = @user.bot?
+
     # User is authenticated
     authenticated? &&
-    # Have to be a basic level at least -- and now: OR SENDING TO ADMIN
-    (is_group || @user.has_trust_level?(SiteSetting.min_trust_to_send_messages) || notify_moderators || target.admin) &&
-    # User disabled private message
-    (is_staff? || is_group || target.user_option.allow_private_messages) &&
-    # PMs are enabled
-    (is_staff? || SiteSetting.enable_personal_messages || notify_moderators) &&
-    # Can't send PMs to suspended users
-    (is_staff? || is_group || !target.suspended?) &&
-    # Check group messageable level
-    (is_staff? || is_user || Group.messageable(@user).where(id: target.id).exists?) &&
-    # Silenced users can only send PM to staff
-    (!is_silenced? || target.staff?)
+      # User can send PMs, this can be covered by trust levels as well via AUTO_GROUPS
+      (
+        is_staff? || from_bot || from_system ||
+          (@user.in_any_groups?(SiteSetting.personal_message_enabled_groups_map)) ||
+          notify_moderators || SiteSetting.allow_pm_to_staff_enabled 
+      )
+      
+  end
+
+  add_to_class(:guardian, :can_send_private_message?) do |target , notify_moderators: false|
+    target_is_user = target.is_a?(User)
+    target_is_group = target.is_a?(Group)
+    from_system = @user.is_system_user?
+
+    # Must be a valid target
+    return false if !(target_is_group || target_is_user)
+
+    # Users can send messages to certain groups with the `everyone` messageable_level
+    # even if they are not in personal_message_enabled_groups
+    group_is_messageable = target_is_group && Group.messageable(@user).where(id: target.id).exists?
+
+    receiving_group = (target.groups.pluck(:name) & SiteSetting.allow_pm_allowed_pm_groups.split("|")).length > 0
+    # User is authenticated and can send PMs, this can be covered by trust levels as well via AUTO_GROUPS
+    (can_send_private_messages?(notify_moderators: notify_moderators) || group_is_messageable) &&
+      # User disabled private message
+      (is_staff? || target_is_group || (target.user_option.allow_private_messages &&
+        receiving_group )) &&
+      # Can't send PMs to suspended users
+      (is_staff? || target_is_group || !target.suspended?) &&
+      # Check group messageable level
+      (from_system || target_is_user || group_is_messageable || notify_moderators) &&
+      # Silenced users can only send PM to staff
+      (!is_silenced? || target.staff?)
   end
 end
--- spec/allow_pms_spec.rb
+++ spec/allow_pms_spec.rb
@@ -4,17 +4,21 @@ require "rails_helper"
 
 describe TopicCreator do
   fab!(:user0)      { Fabricate(:user, trust_level: TrustLevel[0]) }
+  fab!(:normal_user) { Fabricate(:user, trust_level: TrustLevel[1], admin: false, moderator: false)}
   fab!(:user2)      { Fabricate(:user, trust_level: TrustLevel[2]) }
   fab!(:admin)     { Fabricate(:admin) }
   fab!(:moderator)     { Fabricate(:moderator) }
 
   let(:pm_valid_attrs_to_admin)  { { raw: 'this is a new post', title: 'this is a new title', archetype: Archetype.private_message, target_usernames: admin.username } }
   let(:pm_valid_attrs)  { { raw: 'this is a new post', title: 'this is a new title', archetype: Archetype.private_message, target_usernames: moderator.username } }
+  let(:pm_to_normal_user)  { { raw: 'this is another new post', title: 'this is still a new title', archetype: Archetype.private_message, target_usernames:  user2.username} }
 
   context 'personal message' do
     it "should be possible for a trusted user to send private message" do
+      puts "setting the settings"
       SiteSetting.min_trust_to_send_messages = TrustLevel[2]
       SiteSetting.enable_personal_messages = true
+      SiteSetting.allow_pm_allowed_pm_groups="staff"
       expect(TopicCreator.create(user2, Guardian.new(user2), pm_valid_attrs)).to be_valid
     end
 
@@ -28,7 +32,7 @@ describe TopicCreator do
       SiteSetting.min_trust_to_send_messages = TrustLevel[4]
       SiteSetting.enable_personal_messages = true
       expect do
-        TopicCreator.create(user0, Guardian.new(user0), pm_valid_attrs)
+        TopicCreator.create(user0, Guardian.new(normal_user), pm_to_normal_user)
       end.to raise_error(ActiveRecord::Rollback)
     end
   end
