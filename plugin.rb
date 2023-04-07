0e726facf8a2ca198b9203012f70312210d7634b
Jay Pfaffman <jay@literatecomputing.com>
2023-04-07 13:13:24 -0500

    really fix for new site settings

--- plugin.rb
+++ plugin.rb
@@ -30,11 +30,26 @@ after_initialize do
       # Silenced users can only send PM to staff
       (!is_silenced? || target.staff?)
   end
+  add_to_class(:guardian, :can_send_private_messages?) do |notify_moderators: false|
+    from_system = @user.is_system_user?
+    from_bot = @user.bot?
+
+    # User is authenticated
+    authenticated? &&
+      # User can send PMs, this can be covered by trust levels as well via AUTO_GROUPS
+      (
+        is_staff? || from_bot || from_system ||
+          (@user.in_any_groups?(SiteSetting.personal_message_enabled_groups_map)) ||
+          notify_moderators || SiteSetting.allow_pm_to_staff_enabled
+      )
+  end
 
   add_to_class(:guardian, :can_send_private_message?) do |target, notify_moderators: false|
     target_is_user = target.is_a?(User)
     target_is_group = target.is_a?(Group)
     from_system = @user.is_system_user?
+    current_user_can_send =
+      current_user.in_any_groups?(SiteSetting.personal_message_enabled_groups_map)
 
     # Must be a valid target
     return false if !(target_is_group || target_is_user)
@@ -42,14 +57,14 @@ after_initialize do
     # Users can send messages to certain groups with the `everyone` messageable_level
     # even if they are not in personal_message_enabled_groups
     group_is_messageable = target_is_group && Group.messageable(@user).where(id: target.id).exists?
-
     receiving_group =
       (
         if target_is_group
           false
         else
-          (target.groups.pluck(:name) & SiteSetting.allow_pm_allowed_pm_groups.split("|")).length >
-            0
+          allowed_groups_count =
+            (target.groups.pluck(:name) & SiteSetting.allow_pm_allowed_pm_groups.split("|")).length
+          (allowed_groups_count > 0)
         end
       )
 
@@ -57,7 +72,7 @@ after_initialize do
     (can_send_private_messages?(notify_moderators: notify_moderators) || group_is_messageable) &&
       # User disabled private message
       (
-        is_staff? || target_is_group ||
+        current_user_can_send || is_staff? || target_is_group ||
           (target.user_option.allow_private_messages && receiving_group)
       ) &&
       # Can't send PMs to suspended users
--- spec/allow_pms_spec.rb
+++ spec/allow_pms_spec.rb
@@ -7,21 +7,22 @@ describe TopicCreator do
   fab!(:normal_user) do
     Fabricate(:user, trust_level: TrustLevel[1], admin: false, moderator: false)
   end
+  fab!(:group) { Fabricate(:group) }
   fab!(:user2) { Fabricate(:user, trust_level: TrustLevel[2]) }
   fab!(:admin) { Fabricate(:admin) }
   fab!(:moderator) { Fabricate(:moderator) }
 
   let(:pm_valid_attrs_to_admin) do
     {
-      raw: "this is a new post",
+      raw: "this is a new post to a regular user",
       title: "this is a new title",
       archetype: Archetype.private_message,
       target_usernames: admin.username,
     }
   end
-  let(:pm_valid_attrs) do
+  let(:pm_to_moderator) do
     {
-      raw: "this is a new post",
+      raw: "this is a new post to a moderator",
       title: "this is a new title",
       archetype: Archetype.private_message,
       target_usernames: moderator.username,
@@ -32,36 +33,35 @@ describe TopicCreator do
       raw: "this is another new post",
       title: "this is still a new title",
       archetype: Archetype.private_message,
-      target_usernames: user2.username,
+      target_usernames: normal_user.username,
     }
   end
 
   context "when sending a personal message" do
     it "should be possible for a trusted user to send private message" do
-      puts "setting the settings"
-      SiteSetting.min_trust_to_send_messages = TrustLevel[2]
+      SiteSetting.personal_message_enabled_groups = group.id
+      GroupUser.create(user: user2, group: group)
       SiteSetting.enable_personal_messages = true
-      SiteSetting.allow_pm_allowed_pm_groups = "staff"
-      expect(TopicCreator.create(user2, Guardian.new(user2), pm_valid_attrs)).to be_valid
+      expect(TopicCreator.create(user2, Guardian.new(user2), pm_to_normal_user)).to be_valid
     end
 
     it "should be possible for a new user to send private message to admin" do
-      SiteSetting.min_trust_to_send_messages = TrustLevel[4]
+      SiteSetting.personal_message_enabled_groups = Group::AUTO_GROUPS[:staff]
       SiteSetting.enable_personal_messages = true
       expect(TopicCreator.create(user0, Guardian.new(user0), pm_valid_attrs_to_admin)).to be_valid
     end
 
     it "should not be possible for a new user to send private message to normal user" do
-      SiteSetting.min_trust_to_send_messages = TrustLevel[4]
+      staff = Group.find_by(name: "staff")
+      SiteSetting.min_trust_to_send_messages = staff.id
       SiteSetting.enable_personal_messages = true
       expect do
         TopicCreator.create(user0, Guardian.new(normal_user), pm_to_normal_user)
       end.to raise_error(ActiveRecord::Rollback)
     end
-
-    it "can read a group page" do
-      get "/g/admins"
-      expect(response.status).to eq(200)
-    end
+    # it "can read a group page" do
+    #   get "/g/admins.json"
+    #   expect(response.status).to eq(200)
+    # end
   end
 end
